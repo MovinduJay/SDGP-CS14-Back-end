8cb51f36e9719d36492eef1e2ab40dfde1215b8d
Senuda004 <senudamusic120@gmail.com>
2024-03-09 22:12:40 +0530

    Delete unused image file and modify webapp.py to enable running the Flask app

--- Nutrimate/webapp.py
+++ Nutrimate/webapp.py
@@ -112,9 +112,9 @@ def predict():
     
     return jsonify({'error': 'Invalid request'})
 
-# if __name__ == "__main__":
-#     parser = argparse.ArgumentParser(description="Flask app exposing yolov5 models")
-#     parser.add_argument("--port", default=5000, type=int, help="port number")
-#     args = parser.parse_args()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Flask app exposing yolov5 models")
+    parser.add_argument("--port", default=5000, type=int, help="port number")
+    args = parser.parse_args()
 
-#     app.run(host="0.0.0.0", port=args.port)  # debug=True causes Restarting with stat
+    app.run(host="0.0.0.0", port=args.port)  # debug=True causes Restarting with stat
