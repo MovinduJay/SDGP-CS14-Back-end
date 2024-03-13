0d94db757aae41d63e421d1a16cc088981e3f368
Anuththara Kannangara <anuththara.20221230@iit.ac.lk>
2024-03-13 19:20:39 +0530

    set up mongoDB

---
+++ calDB/index.js
@@ -0,0 +1,20 @@
+var Express = require("express");
+var Mongoclient=require ("mongodb"). MongoClient; 
+var cors=require("cors"); 
+const multer=require ("multer");
+
+var app=Express();
+app.use(cors());
+
+var CONNECTION_STRING="";
+
+var DATABASENAME=" todoappdb" ;
+var database;
+
+app. listen (5038, ()=>{
+
+    Mongoclient. connect (CONNECTION_STRING, (error, client)=>{
+        database=client.db (DATABASENAME);
+        console.log("Mongo DB Connection Successful")
+    })
+})
