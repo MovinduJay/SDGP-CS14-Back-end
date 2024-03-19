e41683ee7df084f87d400d5153d687b216ce9be2
Anuththara Kannangara <anuththara.20221230@iit.ac.lk>
2024-03-20 01:23:22 +0530

    final changes

--- calDB/calMeter/RoutesCal.js
+++ calDB/calMeter/RoutesCal.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const router = express.Router();
 const CalModel = require('./modelsCal/CalModel');
@@ -16,41 +15,29 @@ router.get('/caldata', async (req, res) => {
       res.status(500).json({ error: 'Internal Server Error' });
     }
   });
-  
-  // POST route to add new calData
-  router.post('/caldata', async (req, res) => {
-    try {
-      // Extract data from request body
-      const { uid } = req.body;
-  
-      // Fetch scanned_items from User-Data based on uid
-      const userData = await UserModel.findOne({ uid });
-      const {scanned_items} = userData; // Assuming scanned_items is a field in User-Data schema
-  
-      // Extract other data from request body
-      const { id, goal,consumedCal, date } = req.body;
-  
-      // Create a new calData document
-      const newCalData = new CalModel({
-        uid,
-        scanned_items,
-        id,
-        goal,
-        consumedCal,
-        date
-      });
-      
-  
-      // Save the new calData document to the database
-      await newCalData.save();
-  
-      res.status(201).json(newCalData);
-    } catch (error) {
-        console.log(req.body.goal);
-      console.error('Error adding cal data:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
-  
+
+// POST route to add new calData
+router.post('/caldata', async (req, res) => {
+  try {
+    // Extract data from request body
+    const { uid, goal, consumed, date } = req.body;
+
+    // Create a new calData document
+    const newCalData = new CalModel({
+      uid,
+      goal,
+      consumed,
+      date
+    });
+
+    // Save the new calData document to the database
+    await newCalData.save();
+
+    res.status(201).json(newCalData);
+  } catch (error) {
+    console.error('Error adding cal data:', error);
+    res.status(500).json({ error: 'Internal Server Error', details: error.message});
+  }
+});
   module.exports = router;
   
